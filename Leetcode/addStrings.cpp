a0c151a875eb0cfc72c962a25374cdde21402808
Jonatan-npe <jonatan.garcia-v@mail.escuelaing.edu.co>
2025-03-13 18:38:57 -0500

    Add initial implementation of addStrings function in Leetcode project

---
+++ Leetcode/addStrings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        
+    }
+};
+
+int main(int, char **)
+{
+    cout << "Hello\n";
+    Solution mySol;
+    vector<int> myVec = {0, 1, 5, 2, 3, 4, 6, 8};
+
+    return 0;
+}
