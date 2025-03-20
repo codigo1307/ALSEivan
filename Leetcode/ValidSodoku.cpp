aac7bcf68f241a6038159382e3ff23d6c62d9856
codigo1307 <ivan.carpentier-r@mail.escuelaing.edu.co>
2025-03-20 13:20:03 -0500

    Add initial implementation of isValidSudoku function in ValidSudoku.cpp

---
+++ Leetcode/ValidSodoku.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+    public:
+        bool isValidSudoku(vector<vector<char>>& board) {
+            
+            
+        }
+};
+int main(int, char **)
+{
+    Solution mySol; // Crear una instancia de la clase Solution
+    
+}
